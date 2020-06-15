18f30b9e4e74c016d074ac9041418b20e22074cb
dominik.kolesar <dominik.kolesar@visma.com>
2020-06-15 09:09:09 +0200

    first commit

---
+++ MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,117 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // TODO: Add member variables here:
+    private FirebaseAuth mAuth;
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private  Button mLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.login_email);
+        mPasswordView = (EditText) findViewById(R.id.login_password);
+
+        mLoginButton = findViewById(R.id.login_sign_in_button);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+//        mLoginButton.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+//
+//                    attemptLogin();
+//                    return true;
+//
+//            }
+//        });
+
+        // TODO: Grab an instance of FirebaseAuth
+        mAuth= FirebaseAuth.getInstance();
+
+
+    }
+
+    // Executed when Sign in button pressed
+    public void signInExistingUser(View v)   {
+        // TODO: Call attemptLogin() here
+
+    }
+
+    // Executed when Register button pressed
+    public void registerNewUser(View v) {
+        Intent intent = new Intent(this, com.example.myapplication.RegisterActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
+    // TODO: Complete the attemptLogin() method
+    private void attemptLogin() {
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        if(email.equals("") || password.equals("")){
+            return;
+        }
+        Toast.makeText(this,"Login...",Toast.LENGTH_SHORT);
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(!task.isSuccessful()){
+                    System.out.println("Login not successful "+ task.getException());
+                    ShowErrorDialog();
+                }else{
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+
+    }
+
+    // TODO: Show error on screen with an alert dialog
+
+    private void ShowErrorDialog(){
+        // 1. Instantiate an <code><a href="/reference/android/app/AlertDialog.Builder.html">AlertDialog.Builder</a></code> with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(R.string.login_dialog_message)
+                .setTitle(R.string.login_dialog_title);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private String mDisplayName;
+    private ListView mChatListView;
+    private EditText mInputText;
+    private ImageButton mSendButton;
+
+    private DatabaseReference mDatabaseReference;
+    private ChatListAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupDisplayedName();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mInputText = (EditText) findViewById(R.id.messageInput);
+        mSendButton = (ImageButton) findViewById(R.id.sendButton);
+        mChatListView = (ListView) findViewById(R.id.chat_list_view);
+
+        mInputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                sendMessage();
+                return true;
+            }
+        });
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+
+    }
+
+
+    private void setupDisplayedName() {
+        SharedPreferences prefs = getSharedPreferences(RegisterActivity.CHAT_PREFS, MODE_PRIVATE);
+        mDisplayName = prefs.getString(RegisterActivity.DISPLAY_NAME_KEY, null);
+        if (mDisplayName == null) {
+            mDisplayName = "";
+        }
+
+    }
+
+
+    private void sendMessage() {
+        System.out.println("send message");
+        String input = mInputText.getText().toString();
+        if(!input.equals("")){
+            InstantMessage message = new InstantMessage(input,mDisplayName);
+            mDatabaseReference.child("messages").push().setValue(message);
+            mInputText.setText("");
+        }
+
+    }
+
+    // TODO: Override the onStart() lifecycle method. Setup the adapter here.
+
+    @Override
+    public void onStart() {
+       super.onStart();
+       mAdapter = new ChatListAdapter(this,mDatabaseReference,mDisplayName);
+       mChatListView.setAdapter(mAdapter);
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mAdapter.cleanup();
+    }
+}
