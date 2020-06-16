b0b7d5268a5bd9135af2c64f8c3e305405b2403e
dominik.kolesar <dominik.kolesar@visma.com>
2020-06-16 10:45:38 +0200

    nav

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,29 +1,26 @@
 package com.example.myapplication;
 
-import android.content.Intent;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.bottomnavigation.BottomNavigationMenu;
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
 
 
 
@@ -33,11 +30,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigationView);
-//        BottomNavigationView.OnNavigationItemSelectedListener()
-//        Menu menu = bottomNavigationView.getMenu();
-//        this.onNavigationItemSelected(menu.findItem(R.id.action_favorites));
         bottomNavigation.setOnNavigationItemSelectedListener(navListener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new Login()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new LoginFragment()).commit();
+
 
 
     }
@@ -47,18 +42,23 @@ public class MainActivity extends AppCompatActivity {
             Fragment selectedFragment = null;
             switch(menuItem.getItemId()){
                 case R.id.register:
-                    selectedFragment = new Register();
+                    selectedFragment = new RegisterFragment();
                     break;
                 case R.id.login:
-                    selectedFragment = new Login();
+                    selectedFragment = new LoginFragment();
                     break;
                 case R.id.photo:
-                    selectedFragment = new Photo();
+                    selectedFragment = new PhotoFragment();
+                    break;
+                case R.id.webview:
+                    selectedFragment = new WebviewFragment();
                     break;
             }
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
             return true;
         };
     };
+
+
 
 }
